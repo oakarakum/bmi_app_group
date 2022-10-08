ebc8e3250fb0bff8b24033579913a2d1f269c267
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-10-08 16:07:02 +0300

    TextFormField'lar yapıldı.

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -20,6 +21,12 @@ class _FirstScreenState extends State<FirstScreen> {
   final Color textRenk1= Colors.white;
   final Color textRenk2= Colors.black;
  // int gender_index = 0;
+ double weight = 0;
+  double height = 0;
+  int age = 0;
+  bool agecont = true;
+  bool heightcont = true;
+  bool weightcont = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +74,113 @@ class _FirstScreenState extends State<FirstScreen> {
                   });
                 }), indexx == cinsiyet.other ? textRenk1 : textRenk2,),
               ],
-            )
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              onChanged: ((value) {
+                age = int.parse(value);
+              }),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(3),
+              ],
+              keyboardType: TextInputType.number,
+              validator: (age) {
+                    if (age!.isEmpty) {
+                      agecont == false;
+                      return 'Age section cannot be empty';
+                    } else if (int.parse(age) < 1) {
+                      agecont == false;
+                      return 'Age section cannot be lower than 1';
+                    } else if (int.parse(age) > 120) {
+                      agecont == false;
+                      return 'Age section cannot be higher than 120';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    labelText: "Age",
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 16,),
+                    hintText: "Age",
+                    hintStyle: TextStyle(color: Color.fromARGB(255, 112, 109, 109),fontSize: 16,),
+                  ),
+            ),
+            TextFormField(
+                  onChanged: (value) {
+                    height = double.parse(value);
+                  },
+                  inputFormatters: [
+                    
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(3),
+                  ],
+                  validator: (height) {
+                    if (height!.isEmpty) {
+                      heightcont == false;
+                      return 'Height section cannot be empty';
+                    } else if (int.parse(height) < 50) {
+                      heightcont == false;
+                      return 'Height section cannot be lower than 50';
+                    } else if (int.parse(height) > 220) {
+                      heightcont == false;
+                      return 'Height section cannot be higher than 220';
+                    } else {
+                      return null;
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      labelText: "Height",
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 16),
+                      suffixText: "cm",
+                      suffixStyle:
+                          TextStyle(color: Color.fromARGB(255, 112, 109, 109)),
+                      hintText: "Height",
+                      hintStyle: TextStyle(
+                          color: Color.fromARGB(255, 112, 109, 109),
+                          fontSize: 16)),
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    weight = double.parse(value);
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(3),
+                  ],
+                  validator: (weight) {
+                    if (weight!.isEmpty) {
+                      heightcont == false;
+                      return 'Weight section cannot be empty';
+                    } else if (int.parse(weight) < 3) {
+                      heightcont == false;
+                      return 'Weight section cannot be lower than 3';
+                    } else if (int.parse(weight) > 300) {
+                      heightcont == false;
+                      return 'Weight section cannot be higher than 300';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      labelText: "Weight",
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 16),
+                      suffixText: "kg",
+                      suffixStyle:
+                          TextStyle(color: Color.fromARGB(255, 112, 109, 109)),
+                      hintText: "Weight",
+                      hintStyle: TextStyle(
+                          color: Color.fromARGB(255, 112, 109, 109),
+                          fontSize: 16)),
+                ),
           ],
         ),
       ),
