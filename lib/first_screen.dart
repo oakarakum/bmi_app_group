b95c3d7d4951113f7ea51bbc63b25114f4cd3447
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-10-08 16:59:27 +0300

    Calculate button yapıldı.

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,9 +1,13 @@
+
+
+import 'package:bmi_app_group/bmicalc.dart';
+import 'package:bmi_app_group/second_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-enum cinsiyet {female, male, other}
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
@@ -12,104 +16,135 @@ class FirstScreen extends StatefulWidget {
   State<FirstScreen> createState() => _FirstScreenState();
 }
 
+
+
 class _FirstScreenState extends State<FirstScreen> {
-   cinsiyet? indexx;
-  final Color aktif = Colors.green;
-  final Color inaktif = Colors.white;
-  final Color kirmizi = Colors.red;
-  final Color mor = Colors.purple;
-  final Color textRenk1= Colors.white;
-  final Color textRenk2= Colors.black;
- // int gender_index = 0;
- double weight = 0;
+  int gender_index = 0;
+  double weight = 0;
   double height = 0;
   int age = 0;
   bool agecont = true;
   bool heightcont = true;
   bool weightcont = true;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>(); 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 50),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(left: 25, right: 25, bottom: 30),
+        child: GestureDetector(
+          onTap: () {
+
+
+            if (formKey.currentState!.validate()) {
+              setState(() {
+                formKey.currentState!.save();
+              });
+              CalculateResult calculateResult = CalculateResult(height: height, weight: weight);
+              Navigator.push(context, MaterialPageRoute(builder: ((context) => SecondScreen(
+                bmiC: calculateResult.bmi_calc(),
+                cond: calculateResult.condition(),
+                adv: calculateResult.advice(),
+                ftp: calculateResult.foto()))));
+
+            }
+          },
+          child: Container(
+            height: 50,
+            width: 350,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+            child: Center(
               child: Text(
-                "Let's calculate\nyour current BMI",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+                "Calculate BMI",
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "You can find out whether you are\noverweight, underweight or ideal weight",
-              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
+          ),
+        ),
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              
               children: [
-                genderButton("Female", indexx == cinsiyet.female ? aktif : inaktif, 0, (() {
-                  setState(() {
-                    indexx = cinsiyet.female;
-                  });
-                }), indexx == cinsiyet.female ? textRenk1 : textRenk2,),
-                Spacer(),
-                genderButton("Male", indexx == cinsiyet.male ? kirmizi : inaktif, 1, (() {
-                  setState(() {
-                    indexx = cinsiyet.male;
-                  });
-                }), indexx == cinsiyet.male ? textRenk1 : textRenk2,),
-                Spacer(),
-                genderButton("Other", indexx == cinsiyet.other ? mor : inaktif, 2, (() {
-                  setState(() {
-                    indexx = cinsiyet.other;
-                  });
-                }), indexx == cinsiyet.other ? textRenk1 : textRenk2,),
-              ],
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextFormField(
-              onChanged: ((value) {
-                age = int.parse(value);
-              }),
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                LengthLimitingTextInputFormatter(3),
-              ],
-              keyboardType: TextInputType.number,
-              validator: (age) {
-                    if (age!.isEmpty) {
+                SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        "Let's calculate \nyour current BMI",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 40),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    "You can find out whether you are overweight, underweight or ideal weight.",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  
+                  children: [
+                    genderbutton("Female", Color.fromARGB(255, 223, 3, 76), 0),
+                    genderbutton("Male", Colors.blue, 1),
+                    genderbutton("Other", Colors.purple, 2),
+                  ],
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  onChanged: (value) {
+                    age = int.parse(value);
+                  },
+                  inputFormatters: [
+                    
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(3),
+                  ],
+                  keyboardType: TextInputType
+                      .number, 
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       agecont == false;
-                      return 'Age section cannot be empty';
-                    } else if (int.parse(age) < 1) {
+                      return 'Age cannot be empty';
+                    } else if (int.parse(value) < 1) {
                       agecont == false;
-                      return 'Age section cannot be lower than 1';
-                    } else if (int.parse(age) > 120) {
+                      return 'Age cannot be lower than 1';
+                    } else if (int.parse(value) > 120) {
                       agecont == false;
-                      return 'Age section cannot be higher than 120';
+                      return 'Age cannot be higher than 120';
                     } else {
                       return null;
                     }
                   },
+
                   decoration: InputDecoration(
-                    floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    labelText: "Age",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 16,),
-                    hintText: "Age",
-                    hintStyle: TextStyle(color: Color.fromARGB(255, 112, 109, 109),fontSize: 16,),
-                  ),
-            ),
-            TextFormField(
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      labelText: "Age",
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 16),
+                      hintText: "Age",
+                      hintStyle: TextStyle(
+                          color: Color.fromARGB(255, 112, 109, 109),
+                          fontSize: 16)),
+                ),
+                TextFormField(
                   onChanged: (value) {
                     height = double.parse(value);
                   },
@@ -118,16 +153,16 @@ class _FirstScreenState extends State<FirstScreen> {
                     FilteringTextInputFormatter.digitsOnly,
                     LengthLimitingTextInputFormatter(3),
                   ],
-                  validator: (height) {
-                    if (height!.isEmpty) {
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       heightcont == false;
-                      return 'Height section cannot be empty';
-                    } else if (int.parse(height) < 50) {
+                      return 'Height cannot be empty';
+                    } else if (int.parse(value) < 50) {
                       heightcont == false;
-                      return 'Height section cannot be lower than 50';
-                    } else if (int.parse(height) > 220) {
+                      return 'Height cannot be lower than 50';
+                    } else if (int.parse(value) > 220) {
                       heightcont == false;
-                      return 'Height section cannot be higher than 220';
+                      return 'Height cannot be higher than 220';
                     } else {
                       return null;
                     }
@@ -146,28 +181,25 @@ class _FirstScreenState extends State<FirstScreen> {
                           fontSize: 16)),
                 ),
                 TextFormField(
-                  onChanged: (value) {
-                    weight = double.parse(value);
+                  onChanged: (height) {
+                    weight = double.parse(height);
                   },
                   keyboardType: TextInputType.number,
                   inputFormatters: [
-                    
                     FilteringTextInputFormatter.digitsOnly,
                     LengthLimitingTextInputFormatter(3),
                   ],
-                  validator: (weight) {
-                    if (weight!.isEmpty) {
+                  validator: (value) {
+                    if (value!.isEmpty) {
                       heightcont == false;
-                      return 'Weight section cannot be empty';
-                    } else if (int.parse(weight) < 3) {
+                      return 'Weight cannot be empty';
+                    } else if (int.parse(value) < 3) {
                       heightcont == false;
-                      return 'Weight section cannot be lower than 3';
-                    } else if (int.parse(weight) > 300) {
+                      return 'Weight cannot be lower than 3';
+                    } else if (int.parse(value) > 300) {
                       heightcont == false;
-                      return 'Weight section cannot be higher than 300';
-                    } else {
-                      return null;
-                    }
+                      return 'Weight cannot be higher than 300';
+                    } else {}
                   },
                   decoration: InputDecoration(
                       floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -181,44 +213,51 @@ class _FirstScreenState extends State<FirstScreen> {
                           color: Color.fromARGB(255, 112, 109, 109),
                           fontSize: 16)),
                 ),
-          ],
+                
+              ],
+            ),
+          ),
         ),
       ),
     );
-    
   }
-  /* void changedIndex(int index){
+
+  void changeIndex(int index) {
     setState(() {
       gender_index = index;
     });
-  } */
-
-  InkWell genderButton(String gender, Color bordercolor, int index,  Function() asd,Color textcolorr) {
-    return InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: asd /* (() {
-                  setState(() {
-                    indexx  = asd;
-                    /* gender_index = index; */
-                  });
-                }) */,
-                child: Container(
-                  height: 50,
-                  width: 105,
-                  decoration: BoxDecoration(
-                    color: bordercolor,/* gender_index == index ? color : Colors.white, */
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 0.7
-                    )
-                    
-                  ),
-                  child: Center(
-                    child: Text(gender, style: TextStyle(fontSize: 17, color: textcolorr),),
-                  ),
-                ),
-              );
   }
 
+  GestureDetector genderbutton(String value, Color color, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          changeIndex(index);
+        });
+      },
+      child: Container(
+        height: 55,
+        width: 110,
+        decoration: BoxDecoration(
+            color: gender_index == index
+                ? color
+                : Colors
+                    .white, //kısa if-else koy arka plan tıklandığında siyah rengi alması için
+            border: Border.all(
+                color: index == 0
+                    ? Color.fromARGB(255, 203, 198, 198)
+                    : Color.fromARGB(255, 182, 176, 176)),
+            borderRadius: BorderRadius.circular(100)),
+        child: Center(
+          child: Text(
+            value,
+            style: TextStyle(
+                color: gender_index == index ? Colors.white : Colors.black,
+                fontSize:
+                    15), //kısa if-else koy arka plan tıklandığında siyah rengi alması için
+          ),
+        ),
+      ),
+    );
+  }
 }
